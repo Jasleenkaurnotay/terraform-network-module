a12156f40fea9cce03bf54b51c125fcb9ec38dd3
JasK-Git <jasleen.notay@yahoo.com>
2026-06-06 17:20:37 +0530

    Adding private subnet, pvt routing table, NAT gateway

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_vpc" "vpc" {
     enable_dns_hostnames = true
     enable_dns_support = true
 
-    tags = local.common_tags
+    tags = merge(local.common_tags, {Name = "${var.vpc_name}"})
 }
 
 # Create public subnets
@@ -20,7 +20,7 @@ resource "aws_subnet" "pub_subnet" {
     cidr_block = each.key
     availability_zone = each.value.availability_zone
 
-    tags = local.common_tags
+    tags = merge(local.common_tags, {Name = "${var.vpc_name}-${each.value.prefix}"})
 }
 
 # Create private subnets
@@ -34,15 +34,14 @@ resource "aws_subnet" "pvt_subnet" {
     cidr_block = each.key
     availability_zone = each.value.availability_zone
 
-    tags = local.common_tags
+    tags = merge(local.common_tags, {Name = "${var.vpc_name}-${each.value.prefix}"})
 }
 
 # Create internet gateway
 resource "aws_internet_gateway" "igw" {
-    for_each = local.common_tags
     vpc_id = aws_vpc.vpc.id
 
-    tags = merge(local.common_tags, {Name = "${var.vpc_name}-${each.value.}})
+    tags = merge(local.common_tags, {Name = "${var.vpc_name}-igw"})
 }
 
 # Create public route table
@@ -56,6 +55,63 @@ resource "aws_route_table" "pub_rt" {
         gateway_id = aws_internet_gateway.igw.id
     }
 
-    tags = local.common_tags
+    tags = merge(local.common_tags, {Name = "${var.vpc_name}-public-rt"})
 }
 
+# Create route table association
+resource "aws_route_table_association" "pub_rt_asst" {
+    for_each = local.public_subnet_map
+    subnet_id = aws_subnet.pub_subnet[each.key].id
+    route_table_id = aws_route_table.pub_rt.id
+}
+
+# Create elastic IP 
+resource "aws_eip" "nat_gw_eip" {
+    # We need to allocate as many EIPs as there are NAT gateways
+    count = local.nat_count     # Requests multiple EIPs
+    depends_on = [ aws_internet_gateway.igw ]
+    domain = "vpc"
+    tags = merge(local.common_tags, { Name = "${var.vpc_name}-eip-${count.index + 1}"})
+}
+
+# Create NAT gateway
+resource "aws_nat_gateway" "nat_gw" {
+    count = local.nat_count
+    connectivity_type = "public"
+    allocation_id = aws_eip.nat_gw_eip[count.index].id
+    vpc_id = aws_vpc.vpc.id
+    subnet_id = aws_subnet.pub_subnet[keys(local.public_subnet_map)[count.index]].id    # Use keys function to convert public subnet map data into list for count to loop over
+    depends_on = [ aws_internet_gateway.igw ]
+
+    tags = merge(local.common_tags, { Name = "${var.vpc_name}-nat-gw-${count.index + 1}"})
+}
+
+# Create public route tables - one per AZ
+resource "aws_route_table" "pvt_rt" {
+    for_each = local.nat_count > 0 ? local.private_subnet_map : {} 
+    # If nat_count = 0, there are no NAT gateways. In that scenario, we should not be creating the below mentioned route
+
+    vpc_id = aws_vpc.vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.nat_gw[ var.need_single_nat_gateway ? 0 : index( keys(local.private_subnet_map), each.key )].id
+        # If single NAT gateway, always use NATGateway[0], if HA, use the NAT gateway at the same position as this subnet in the map
+    }
+
+    tags = merge(local.common_tags, {Name = "${var.vpc_name}-private-${each.value.availability_zone}-rt"})
+}
+
+# Create route table association
+resource "aws_route_table_association" "pvt_rt_asst" {
+    for_each = local.nat_count > 0 ? local.private_subnet_map : {}
+    # pvt_rt only exits when nat_count > 0. If there is no NAT, pvt_rt doesn't exits - so referencing it in the association crashes
+
+    subnet_id = aws_subnet.pvt_subnet[each.key].id
+    route_table_id = aws_route_table.pvt_rt[each.key].id
+}
+# For nat gateway:
+# 1. EIP
+# 2. Create nat gateway in a particular VPC in public subnet
+# 3. Allocate EIP
+# 4. Update pvt route table to use NAT gateway
